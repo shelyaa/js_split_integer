20e1103e7f1428736802c6402d3c63895530c5d8
Aliona <ashelyaa17@gmail.com>
2025-09-19 00:49:57 +0300

    task

--- src/splitInteger.test.js
+++ src/splitInteger.test.js
@@ -1,6 +1,18 @@
-'use strict';
+"use strict";
 
-const splitInteger = require('./splitInteger');
+const splitInteger = require("./splitInteger");
+
+function assertProperties(result, value, numberOfParts) {
+  expect(result).toHaveLength(numberOfParts);
+
+  expect(result.every(Number.isInteger)).toBe(true);
+
+  expect(result.reduce((a, b) => a + b, 0)).toBe(value);
+
+  expect(result).toEqual(result.slice().sort((a, b) => a - b));
+
+  expect(Math.max(...result) - Math.min(...result)).toBeLessThanOrEqual(1);
+}
 
 test(`should split a number into equal parts
   if a value is divisible by a numberOfParts`, () => {
@@ -16,14 +28,23 @@ test(`should return a part equals to a value
   expect(result).toEqual([8]);
 });
 
-test('should sort parts ascending if they are not equal', () => {
+test("should sort parts ascending if they are not equal", () => {
   const result = splitInteger(17, 4);
 
   expect(result).toEqual([4, 4, 4, 5]);
 });
 
-test('should add zeros if value < numberOfParts', () => {
+test("should add zeros if value < numberOfParts", () => {
   const result = splitInteger(2, 4);
 
   expect(result).toEqual([0, 0, 1, 1]);
+});
+
+test("should split 32 into 6 parts as in the example", () => {
+  const value = 32;
+  const numberOfParts = 6;
+  const result = splitInteger(value, numberOfParts);
+
+  expect(result).toEqual([5, 5, 5, 5, 6, 6]);
+  assertProperties(result, value, numberOfParts);
 });
